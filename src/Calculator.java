9c3c5b88a4373bd6871273b8537ae8055100d17d
marko0418 <markonikolic0418@gmail.com>
2022-04-01 13:43:15 +0200

    ciao

---
+++ src/Calculator.java
@@ -0,0 +1,42 @@
+import java.math.BigDecimal;
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Calculator {
+	public static void main(String[] args) {
+		char operator;
+		BigDecimal number1, number2, result = null;
+		
+		Scanner input = new Scanner(System.in);
+		input.useLocale(Locale.US);
+		System.out.println("Chose an operator: +, -, *, %" );
+		operator = input.next().charAt(0);
+		System.out.println("Enter first number");
+		number1 = input.nextBigDecimal();
+		System.out.println("Enter second number");
+		number2 = input.nextBigDecimal();
+		
+		switch(operator){
+			case'+':
+				result = Operation.sum(number1, number2);
+				break;
+			case'-':
+				result = Operation.sub(number1, number2);
+				break;
+			case'*':
+				result = Operation.mul(number1, number2);
+				break;
+			case'/':
+				result = Operation.div(number1, number2);
+				break;
+			case'%':
+				result = Operation.mod(number1, number2);
+				break;
+			default:
+				
+		}
+		input.close();
+		System.out.println(number1.doubleValue() + operator + number2.doubleValue() + " = " + result );
+
+	}
+}
